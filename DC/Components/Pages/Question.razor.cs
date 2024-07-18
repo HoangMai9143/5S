3140bb8eee582d0b0e372a41ebe5926e701e2860
Hoàng Mai <hoang.mai@framas.com>
2024-07-18 16:10:32 +0700

    simplify question page

--- DC/Components/Pages/Question.razor.cs
+++ DC/Components/Pages/Question.razor.cs
@@ -4,31 +4,24 @@ using MudBlazor;
 using DC.Models;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace DC.Components.Pages
 {
   public partial class Question
   {
-    private List<QuestionModel> questions = [];
-    private HashSet<QuestionModel> selectedQuestions = new();
-    private string _selectedItemText = "No row clicked";
-    private bool _selectOnRowClick = false;
-    private bool _selectionChangeable = true;
-    private MudTable<QuestionModel> _table;
+    private List<QuestionModel> questions = new();
+    private bool isLoading = true;
 
     protected override async Task OnInitializedAsync()
     {
-      await LoadQuestions();
-    }
-
-    private async Task LoadQuestions()
-    {
-      questions = await appDbContext.Set<QuestionModel>().ToListAsync();
-    }
-
-    private void OnRowClick(TableRowClickEventArgs<QuestionModel> args)
-    {
-      _selectedItemText = $"{args.Item.QuestionContext} (ID: {args.Item.Id})";
+      try
+      {
+        questions = await appDbContext.Set<QuestionModel>().ToListAsync();
+      }
+      finally
+      {
+        isLoading = false;
+        StateHasChanged();
+      }
     }
   }
 }

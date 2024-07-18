27a1fa03111f785b2884c3b49b70d0852a077f3b
Hoàng Mai <hoang.mai@framas.com>
2024-07-18 12:43:45 +0700

    minor update

--- DC/ViewModels/RegisterViewModel.cs
+++ DC/ViewModels/RegisterViewModel.cs
@@ -8,7 +8,6 @@ namespace DC.ViewModels
     public string Username { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required")]
-    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
     public string Password { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Confirm Password is required")]

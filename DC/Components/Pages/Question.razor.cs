848c981f95c19bc9cb3ffe799e63fc88d7c1a57c
Hoàng Mai <hoang.mai@framas.com>
2024-07-18 13:49:28 +0700

    done showing Question from db

--- DC/Components/Pages/Question.razor.cs
+++ DC/Components/Pages/Question.razor.cs
@@ -1,13 +1,34 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using MudBlazor;
 using DC.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace DC.Components.Pages
 {
   public partial class Question
   {
+    private List<QuestionModel> questions = [];
+    private HashSet<QuestionModel> selectedQuestions = new();
+    private string _selectedItemText = "No row clicked";
+    private bool _selectOnRowClick = false;
+    private bool _selectionChangeable = true;
+    private MudTable<QuestionModel> _table;
+
+    protected override async Task OnInitializedAsync()
+    {
+      await LoadQuestions();
+    }
+
+    private async Task LoadQuestions()
+    {
+      questions = await appDbContext.Set<QuestionModel>().ToListAsync();
+    }
+
+    private void OnRowClick(TableRowClickEventArgs<QuestionModel> args)
+    {
+      _selectedItemText = $"{args.Item.QuestionContext} (ID: {args.Item.Id})";
+    }
   }
 }
--- DC/Data/AppDbContext.cs
+++ DC/Data/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace DC.Data
   public class AppDbContext : DbContext
   {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-    public DbSet<Models.QuestionModel> QuestionModel { get; set; }
+    public DbSet<QuestionModel> QuestionModel { get; set; }
     public DbSet<ResultModel> ResultModel { get; set; }
     public DbSet<StaffModel> StaffModel { get; set; }
     public DbSet<SurveyModel> SurveyModel { get; set; }
